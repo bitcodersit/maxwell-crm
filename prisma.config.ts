25e63dae02ab244b0a3ad03ebe8bb8a88ee4e516
RK Anik <30260735+rkanik@users.noreply.github.com>
2026-04-04 00:42:33 +0600

    initialize prisma

---
+++ prisma.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, env } from 'prisma/config'
+import 'dotenv/config'
+
+const url = `mysql://${env('NUXT_DATABASE_USER')}:${env('NUXT_DATABASE_PASSWORD')}@${env(
+  'NUXT_DATABASE_HOST'
+)}:${env('NUXT_DATABASE_PORT')}/${env('NUXT_DATABASE_NAME')}?ssl-mode=REQUIRED`
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url,
+  },
+})
---
+++ server/utils/db.ts
@@ -0,0 +1,24 @@
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import { PrismaClient } from '@@/prisma/client/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    adapter: new PrismaMariaDb({
+      host: process.env.NUXT_DATABASE_HOST,
+      user: process.env.NUXT_DATABASE_USER,
+      password: process.env.NUXT_DATABASE_PASSWORD,
+      database: process.env.NUXT_DATABASE_NAME,
+      port: Number(process.env.NUXT_DATABASE_PORT),
+    }),
+  })
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
