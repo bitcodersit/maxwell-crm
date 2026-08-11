9b0e716eb91969852d72dc83b3a172a503efbdcc
RK Anik <30260735+rkanik@users.noreply.github.com>
2026-08-11 22:12:43 +0600

    Removed cookie consent

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -12,10 +12,8 @@ withDefaults(
   }
 )
 
-const route = useRoute()
-const toast = useToast()
-
 const open = ref(false)
+const route = useRoute()
 
 const { user } = useCurrentUser()
 const { title, links: panelLinks, isSearchOpen, isNotificationsOpen } = useDashboard()
@@ -224,34 +222,6 @@ const groups = computed(() => [
     ]
   }
 ])
-
-onMounted(async () => {
-  const cookie = useCookie('cookie-consent')
-  if (cookie.value === 'accepted') {
-    return
-  }
-
-  toast.add({
-    title: 'We use first-party cookies to enhance your experience on our website.',
-    duration: 0,
-    close: false,
-    actions: [
-      {
-        label: 'Accept',
-        color: 'neutral',
-        variant: 'outline',
-        onClick: () => {
-          cookie.value = 'accepted'
-        }
-      },
-      {
-        label: 'Opt out',
-        color: 'neutral',
-        variant: 'ghost'
-      }
-    ]
-  })
-})
 
 useHead({ title })
 useUiColors()
